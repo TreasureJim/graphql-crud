f928c6c1292e716e61aada26e068d91ef224f85f
Liam Boreback <boreback.work@gmail.com>
2022-05-18 23:18:34 +0800

    Implementing supertokens for auth

---
+++ config/backendConfig.ts
@@ -0,0 +1,36 @@
+import ThirdPartyNode from "supertokens-node/recipe/thirdparty";
+import SessionNode from "supertokens-node/recipe/session";
+import { appInfo } from "./appInfo";
+import { TypeInput } from "supertokens-node/types";
+import { env } from "process";
+
+export const backendConfig = (): TypeInput => {
+  return {
+    framework: "express",
+    supertokens: {
+      connectionURI: env.SUPERTOKENS_CONNECTIONURI!,
+      apiKey: env.SUPERTOKENS_APIKEY,
+    },
+    appInfo,
+    recipeList: [
+      ThirdPartyNode.init({
+        signInAndUpFeature: {
+          providers: [
+            // We have provided you with development keys which you can use for testsing.
+            // IMPORTANT: Please replace them with your own OAuth keys for production use.
+            ThirdPartyNode.Google({
+              clientId: env.GOOGLE_OAUTH_CLIENTID!,
+              clientSecret: env.GOOGLE_OATH_CLIENTSECRET!,
+            }),
+            // ThirdPartyNode.Github({
+            //   clientId: "467101b197249757c71f",
+            //   clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd",
+            // }),
+          ],
+        },
+      }),
+      SessionNode.init(),
+    ],
+    isInServerlessEnv: true,
+  };
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import SuperTokensReact from "supertokens-auth-react"
+import { frontendConfig } from '../config/frontendConfig'
+
+if (typeof window !== "undefined") {
+	SuperTokensReact.init(frontendConfig())
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
