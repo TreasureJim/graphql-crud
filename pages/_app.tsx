032cdc7f79f11e06097a28df3f5871598bf2e199
Liam Boreback <boreback.work@gmail.com>
2022-05-22 22:00:56 +0800

    Added tailwind css. Started work on main page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import SuperTokensReact from "supertokens-auth-react"
-import { frontendConfig } from '../config/frontendConfig'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import SuperTokensReact from "supertokens-auth-react";
+import { frontendConfig } from "../config/frontendConfig";
 
 if (typeof window !== "undefined") {
-	SuperTokensReact.init(frontendConfig())
+  SuperTokensReact.init(frontendConfig());
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{tsx, ts, js, jsx}",
+    "./components/**/*.{tsx, ts, js, jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  theme: {
+    fontFamily: {
+      sans: ["Darker Grotesque", "sans-serif"],
+    },
+    fontSize: {
+      base: ["1.25rem", "30px"],
+    },
+  },
+};
