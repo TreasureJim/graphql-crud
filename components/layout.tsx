61a3920fc9e8c52cd7332a605839ff80377fddf4
Liam Boreback <boreback.work@gmail.com>
2022-05-23 22:02:37 +0800

    Extracted layout to seperate file Added images to home page

---
+++ components/layout.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function Layout() {
+	return (
+		<div>
+			<div className="flex justify-end items-center h-20 w-full top-0 mx-auto text-right bg-black text-white">
+				<span className="">Find your own space</span>
+				<Link href="/auth/register">
+					<div className="m-5 mr-0 border-2 border-white py-2 px-12 rounded-sm">
+            Register
+					</div>
+				</Link>
+				<Link href="/auth">
+					<div className="m-5 border-2 bg-white text-black py-2 px-12 border-white rounded-sm">
+            Login
+					</div>
+				</Link>
+			</div>
+		</div>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,32 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Layout from "../components/layout";
+
+import logo from "../public/dev-logo.svg";
+import modelLeft from "../public/images/model-left.jpg";
+import modelRight from "../public/images/model-right.jpg";
 
 const Home: NextPage = () => {
   return (
     <>
-      <nav>
-        <div className="flex justify-end items-center h-20 fixed w-full top-0 mx-auto text-right bg-black text-white">
-          <span className="">Start your own shop</span>
-          <button className="m-5 mr-0 border-2 border-white py-2 px-12 rounded-sm">
-            Register
-          </button>
-          <button className="m-5 border-2 bg-white text-black py-2 px-12 border-white rounded-sm">
-            Login
-          </button>
+      <Layout />
+      <div className="absolute z-10 w-full pt-2">
+        <div className="m-auto w-20">
+          <Image src={logo} layout="responsive" alt="logo" />
         </div>
-      </nav>
+      </div>
+
+      <div>
+        <div className="flex flex-row">
+          <div className="w-2/4">
+            <Image src={modelLeft} alt="model left" layout="responsive" />
+          </div>
+          <div className="w-2/4">
+            <Image src={modelRight} alt="model right" layout="responsive" />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
