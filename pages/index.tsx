df4ac12802dc53339618e6a3bb3cd99a2983af42
Liam Boreback <boreback.work@gmail.com>
2022-05-31 05:36:51 +0800

    Fixed image positioning on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,16 +15,16 @@ const Home: NextPage = () => {
 				<Layout />
 			</ClientOnly>
 
-			<div className="flex flex-row absolute bottom-0 w-full cursor-pointer text-7xl overflow-hidden">
+			<div className="flex flex-row absolute bottom-0 w-full h-screen text-7xl overflow-hidden">
 
-				<div className="w-2/4 hover:brightness-75 transition-all duration-300 h-screen">
-					<Image src={modelLeft} alt="model left" layout="responsive" className="right-0" />
+				<div className="w-2/4 transition-all duration-300">
+					<Image src={modelLeft} alt="model left" layout="responsive" className="absolute top-0 right-0" />
 					<div className="absolute bottom-0 left-1">
-						Build
+						Build -- NOT IMPLEMENTED
 					</div>
 				</div>
 				<Link href="/store">
-					<div className="w-2/4 hover:brightness-75 transition-all duration-300">
+					<div className="w-2/4 hover:brightness-75 transition-all duration-300 cursor-pointer">
 						<Image src={modelRight} alt="model right" layout="responsive" />
 						<div className="absolute bottom-0 right-1">
 							Browse
