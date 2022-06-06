46307c91d9f38dedb8a2ca559430f1f06b66c9d6
Liam Boreback <boreback.work@gmail.com>
2022-06-06 21:37:13 +0800

    Added apollo provider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { UserProvider } from "@auth0/nextjs-auth0";
+import { ApolloProvider } from "@apollo/client";
+import apolloClient from "../lib/apollo";
 
+// eslint-disable-next-line require-jsdoc
 function MyApp({ Component, pageProps }: AppProps) {
-
 	return (
 		<UserProvider>
-			<Component {...pageProps} />
+			<ApolloProvider client={apolloClient}>
+				<Component {...pageProps} />
+			</ApolloProvider>
 		</UserProvider>
 	);
 }
