c0e52a7a0ba50990ac4b458a7f9ecedeb5019ec8
Liam Boreback <boreback.work@gmail.com>
2022-06-07 23:14:36 +0800

    Changed graphql query to frontend

--- pages/store/index.tsx
+++ pages/store/index.tsx
@@ -1,25 +1,41 @@
-import { gql } from "@apollo/client";
-import { GetServerSideProps, NextPage } from "next";
+import { gql, useQuery } from "@apollo/client";
+import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../../components/layout";
-import CreateApolloClient from "../../lib/ApolloClient";
-import withApollo from "../../lib/withApollo";
 
-interface IStoresProps {
-	stores: any[];
-}
+const Stores: NextPage = () => {
+	interface IQuery {
+		stores: {
+			id: string;
+			name: string;
+			description: string;
+			imageUrl: string;
+		}[]
+	}
 
-const Stores: NextPage<IStoresProps> = ({ stores }) => {
+	const { data } = useQuery<IQuery>(
+		gql`
+		query Query {
+			stores {
+				id
+				name
+				description
+				imageUrl
+			}
+		}
+	  `,
+	);
+
 	return (
 		<>
 			<Layout />
 			<div className="flex flex-wrap flex-row justify-between">
-				{stores.map(store => (
+				{data?.stores.map((store) => (
 					<Link key={store.id} href={`/store/${store.id}`} >
 						<div className="w-72 m-5 hover:shadow-lg transition rounded-md">
-							{store.image_url &&
-								<Image src={store.image_url} layout="responsive" width="5" height="2" />
+							{store.imageUrl &&
+								<Image src={store.imageUrl} layout="responsive" width="5" height="2" />
 							}
 							<p className="text-center font-bold text-xl">{store.name}</p>
 							<p className="text-center">{store.description}</p>
@@ -28,39 +44,7 @@ const Stores: NextPage<IStoresProps> = ({ stores }) => {
 				))}
 			</div>
 		</>
-	)
-}
-
-export const getServerSideProps: GetServerSideProps<IStoresProps> = async () => {
-	const client = CreateApolloClient({}, {});
-
-	interface IQuery {
-		dbo_store: {
-			id: string;
-			name: string;
-			description?: string;
-			image_url?: string;
-		}[]
-	}
-	const { data } = await client.query<IQuery>({
-		query:
-			gql`
-		query GetStores {
-			dbo_store {
-				description
-				id
-				name
-				image_url
-			  }
-		}
-	  `
-	});
-
-	return {
-		props: {
-			stores: data.dbo_store
-		}
-	}
+	);
 };
 
-export default withApollo(Stores);
+export default Stores;
