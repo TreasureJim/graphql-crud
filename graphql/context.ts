8671803e1d2a562dfcf6225ef077458fe0051572
TreasureJim <boreback.work@gmail.com>
2022-06-19 00:05:32 +0800

    Set graphql context to use next-auth sessions -- UNFINISHED

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,13 +1,22 @@
+import { getSession } from "next-auth/react";
 import { PrismaClient } from "@prisma/client";
 import prisma from "../lib/prisma";
+import { DefaultSession } from "next-auth";
 
 export type Context = {
-  prisma: PrismaClient;
+	user?: DefaultSession["user"];
+	prisma: PrismaClient;
 };
 
 // eslint-disable-next-line require-jsdoc
 export async function createContext({ req, res }: any): Promise<Context> {
+	const session = await getSession({ req: req });
+	if (!session) return { prisma };
+
+	const { user } = session;
+
 	return {
+		user,
 		prisma,
 	};
 }
