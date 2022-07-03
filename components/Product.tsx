02dd9473a440c1ca024a49462615b2527006822a
TreasureJim <boreback.work@gmail.com>
2022-07-03 22:56:27 +0800

    Fix image sizing of product list

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import { FC } from "react"
+import { FC } from "react";
 
 export interface IProduct {
-	id: number;
+	id: string;
 	name: string;
 	price: number;
-	image_url: string;
-	brand: string;
+	imageUrl: string;
 }
 interface IProductProps {
 	product: IProduct;
@@ -14,15 +13,16 @@ interface IProductProps {
 
 export const Product: FC<IProductProps> = ({ product }) => {
 	return (
-		<div className="w-72 hover:shadow-lg m-5 transition rounded-md">
-			{product.image_url &&
-				<Image src={product.image_url} layout="responsive" height="200" width="300" />
-			}
+		<div className="w-72 h-96 relative hover:shadow-lg m-5 transition rounded-md">
+			<div className="relative w-72 h-96">
+				{product.imageUrl &&
+					<Image src={product.imageUrl} layout="fill" />
+				}
+			</div>
 			<div className="ml-2">
-				<p>{product.brand}</p>
 				<p className="font-bold text-xl">{product.name}</p>
 				<p>{product.price} USD</p>
 			</div>
 		</div>
 	);
-}
+};
--- pages/store/[storeId]/index.tsx
+++ pages/store/[storeId]/index.tsx
@@ -1,86 +1,59 @@
-import { gql } from "@apollo/client";
-import { GetServerSideProps, NextPage } from "next";
+import { gql, useQuery } from "@apollo/client";
+import { NextPage } from "next";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Layout from "../../../components/layout";
-import { Product, IProduct } from "../../../components/Product";
-import CreateApolloClient from "../../../lib/ApolloClient";
+import { Product } from "../../../components/Product";
 
-interface IStoreProps {
-	store: {
+interface IStore {
+	storeById: {
 		name: string;
-		products: IProduct[];
-	};
+		description: string;
+		products: {
+			id: string;
+			name: string;
+			price: number;
+			imageUrl: string;
+		}[];
+	}
 }
 
-const StoreId: NextPage<IStoreProps> = ({ store }) => {
+const StoreId: NextPage = () => {
 	const { storeId } = useRouter().query;
+
+	const { data } = useQuery<IStore>(
+		gql`
+		query ($storeId: String!) {
+			storeById(id: $storeId) {
+				name
+				description
+					products {
+					id
+					name
+					price
+					imageUrl
+				}
+			}
+		}`,
+		{
+			variables: {
+				storeId: storeId,
+			},
+		},
+	);
 
 	return (
 		<>
 			<Layout />
-			<p className="text-6xl font-bold text-center">{store.name}</p>
+			<p className="text-6xl font-bold text-center">{data?.storeById.name}</p>
 			<div className="flex flex-wrap flex-row justify-between">
-				{store.products.map(product => (
+				{data?.storeById.products.map((product) => (
 					<Link key={product.id} href={`/store/${storeId}/product/${product.id}`}>
-						<div>
-
-							<Product product={product} />
-						</div>
+						<Product product={product} />
 					</Link>
 				))}
 			</div>
 		</>
 	);
-}
+};
 export default StoreId;
-
-export const getServerSideProps: GetServerSideProps<IStoreProps> = async (ctx) => {
-	const { storeId: storeIdString } = ctx.query;
-	let storeId;
-
-	if (typeof storeIdString != 'string') {
-		ctx.res.statusCode = 400;
-		return {
-			props: { store: {} }
-		}
-	}
-	try {
-		storeId = parseInt(storeIdString);
-	} catch {
-		ctx.res.statusCode = 400;
-		return {
-			props: { store: {} }
-		}
-	}
-
-
-	const graphqlClient = CreateApolloClient({}, {});
-
-	const { data } = await graphqlClient.query({
-		query: gql`
-		query Store($storeId: Int!) {
-			dbo_store_by_pk(id: $storeId) {
-			  name
-			  products {
-				brand
-				description
-				id
-				image_url
-				name
-				price
-			  }
-			}
-		  }					  
-		  `,
-		variables: {
-			storeId: storeId
-		}
-	});
-
-	return {
-		props: {
-			store: data.dbo_store_by_pk
-		}
-	};
-}
