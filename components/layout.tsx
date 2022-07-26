4dbe9f5b3d7f1cfc842904ee2c32686dd14148c4
TreasureJim <boreback.work@gmail.com>
2022-07-26 13:45:40 +0800

    Update visual of layout bar

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable require-jsdoc */
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
-// import Link from "next/link";
-import logo from "../public/dev-logo.svg";
+import Link from "next/link";
+import githubIcon from "../public/images/github-icon.png";
 
 export default function Layout() {
 	function userElement(): JSX.Element {
@@ -13,14 +13,14 @@ export default function Layout() {
 		}
 
 		if (status == "authenticated") {
-			return (<div className="mx-5 flex justify-center items-center">
+			return (<div className="flex justify-center items-center">
+				<span className="mr-2">{session.user?.name}</span>
 				{session.user?.image ?
 					<Image className="inline object-cover w-12 h-12 mr-2 rounded-full" src={session.user.image} layout="fixed" height="42px" width="42px" /> :
 					<div></div>
 				}
-				<span className="ml-2">{session.user?.name}</span>
 				<button onClick={() => signOut()}>
-					<div className="ml-5 bg-white text-black py-2 px-12 hover:bg-gray-100">
+					<div className="ml-5 text-black py-2 px-12 border border-black hover:bg-gray-100 transition">
 						Sign out
 					</div>
 				</button>
@@ -28,7 +28,7 @@ export default function Layout() {
 		} else {
 			return (
 				<button onClick={() => signIn()} >
-					<div className="m-5 border-2 bg-white text-black py-2 px-12 border-white rounded-sm">
+					<div className="m-5 border-2 text-black py-2 px-12 border-white rounded-sm">
 						Login
 					</div>
 				</button>
@@ -36,15 +36,34 @@ export default function Layout() {
 		}
 	}
 
+	function VD(): JSX.Element {
+		return (
+			<div className="h-12 border-l-2 border-black mx-2" />
+		);
+	}
+
 	return (
-		<div className="z-10 relative">
-			<div className="flex items-center h-20 w-full top-0 mx-auto text-right bg-black text-white">
-				<a href="https://github.com/TreasureJim" target="_blank" className="float-left ml-3">
-					<Image src={logo} layout="fixed" width="50" height="50" alt="logo" />
-				</a>
-				<div className="fixed right-0">
-					{userElement()}
+		<div className="flex flex-row items-center justify-between h-20 z-10 mx-14">
+			<Link href={"/"}>
+				<span className="font-bold font-sans text-6xl tracking-widest cursor-pointer">AMELE</span>
+			</Link>
+
+			<div className="inline-flex">
+				<span className="font-bold font-sans text-5xl cursor-pointer">CREATE</span>
+				<VD />
+				<Link href="store">
+					<span className="font-bold font-sans text-5xl cursor-pointer">BROWSE</span>
+				</Link>
+				<VD />
+				<div className="relative h-auto w-12">
+					<a href="https://www.github.com/TreasureJim/graphql-crud" target="blank">
+						<Image src={githubIcon} layout="fill" />
+					</a>
 				</div>
+			</div>
+
+			<div className="relative float-right">
+				{userElement()}
 			</div>
 		</div>
 	);
