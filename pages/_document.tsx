2177db4c76b3299bf072d63fd5f5dd3ca43178d6
TreasureJim <boreback.work@gmail.com>
2022-07-26 13:46:34 +0800

    Import new font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable require-jsdoc */
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -6,10 +7,7 @@ export default function Document() {
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@300;400;500;600;700;800;900&display=swap"
-					rel="stylesheet"
-				/>
+				<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet" />
 			</Head>
 
 			<body>
